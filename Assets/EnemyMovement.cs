d2deebcf4f29ebdadc7821c62d9e966366fd05ce
Cezary-Pokropek <cezary.pokropek@gmail.com>
2019-08-15 12:04:12 +0200

    Modified prototype pathfinding logic

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> path;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(FollowPath());
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Start patrolling");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Patrolling" + " " + waypoint.name);
+            yield return new WaitForSeconds(1f);
+
+        };
+        print("Stop patrolling");
+    }
+}

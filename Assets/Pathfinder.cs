4fdd2bbd0160c1d16b63e62efed8d5a965ca0932
Cezary-Pokropek <cezary.pokropek@gmail.com>
2019-08-16 23:17:17 +0200

    Refactoring pathfinding prototype with Breadth First Search Algorithm

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -5,12 +5,95 @@ using UnityEngine;
 
 public class Pathfinder : MonoBehaviour {
 
-    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    [SerializeField] Waypoint startWaypoint, endWaypoint;
 
-	// Use this for initialization
-	void Start () {
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Queue<Waypoint> queue = new Queue<Waypoint>();
+    bool isRuuning = true;
+    Waypoint searchCenter;
+
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
+    // Use this for initialization
+    void Start()
+    {
         LoadBlocks();
-	}
+        ColorStartAndEnd();
+        Pathfind();
+        //ExploreNeighbours();
+    }
+
+    private void Pathfind()
+    {
+        queue.Enqueue(startWaypoint);
+
+        while (queue.Count > 0 && isRuuning)
+        {
+            searchCenter = queue.Dequeue();
+            // print("Searching from: " + searchCenter);
+            HaltIfEndFound();
+            ExploreNeighbours();
+            searchCenter.isExplored = true;
+        }
+
+        print("Finished pathfinding?");
+    }
+
+    private void HaltIfEndFound()
+    {
+        if (searchCenter == endWaypoint)
+        {
+            //print("searching from end node, therefore stopping");
+            isRuuning = false;
+        }
+    }
+
+    private void ExploreNeighbours()
+    {
+        if (!isRuuning) { return; }
+
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = searchCenter.GetGridPos() + direction;
+            //print("Exploring" + explorationCoordinates);
+            try
+            {
+                QueueNewNeighbours(explorationCoordinates);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
+    }
+
+    private void QueueNewNeighbours(Vector2Int explorationCoordinates)
+    {
+        Waypoint neighbour = grid[explorationCoordinates];
+        if (neighbour.isExplored || queue.Contains(neighbour))
+        {
+            // do nothing
+        }
+        else
+        {
+            queue.Enqueue(neighbour);
+            //print("Queueing" + neighbour);
+            neighbour.exploredFrom = searchCenter;
+        }
+
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.green);
+        endWaypoint.SetTopColor(Color.red);
+    }
 
     private void LoadBlocks()
     {
@@ -28,7 +111,7 @@ public class Pathfinder : MonoBehaviour {
             }
             
         }
-        print("Loaded" + grid.Count + "blocks");
+        //print("Loaded" + grid.Count + "blocks");
     }
 
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour {
 
-    Vector2Int gridPos;
+    // todo consider to set own color in Update()
+    // [SerializeField] Color exploredColor;
 
+    public bool isExplored = false;
+    public Waypoint exploredFrom;
+
+    Vector2Int gridPos;
 
     const int gridSize = 10;
 
@@ -14,14 +19,21 @@ public class Waypoint : MonoBehaviour {
         return gridSize;
     }
 
+    // todo consider to set own color in Update()
+
     public Vector2Int GetGridPos()
     {
         return new Vector2Int
             (
-            Mathf.RoundToInt(transform.position.x / gridSize) * gridSize,
-            Mathf.RoundToInt(transform.position.z / gridSize) * gridSize
+            Mathf.RoundToInt(transform.position.x / gridSize),
+            Mathf.RoundToInt(transform.position.z / gridSize)
             );
     }
 
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
+    }
 
 }
